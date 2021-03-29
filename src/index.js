0e9326dc8b22878fd229e8a618f29de54f490774
thraddash <theartist@gmail.com>
2021-03-29 12:53:16 -0700

    create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+    return <div>Hi there!</div>
+}
+
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
+);
