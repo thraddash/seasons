ee881841df0258134e0073737c33ce58001b422f
thraddash <theartist@gmail.com>
2021-03-29 18:04:40 -0700

    define constructor function to initialize state obj

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    // Require render by React
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
