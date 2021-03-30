0f9f68291ecb6bbaec6812f3bfbc1ef13e8bf09c
thraddash <theartist@gmail.com>
2021-03-29 18:37:20 -0700

    error handling

--- src/index.js
+++ src/index.js
@@ -6,22 +6,28 @@ class App extends React.Component {
         super(props);
 
         // Only time we do direct assignment to this.state
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 // to update state function, use setState
                 this.setState({ lat: position.coords.latitude });
-
-
             },
-            (err) => console.log(err)
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
         );
     }
 
     // Require render by React
     render() {   
-        return <div>Latitude: {this.state.lat}</div>;
+        return (
+        <div>
+            Latitude: {this.state.lat}
+            <br />
+            Error: {this.state.errorMessage}
+        </div>
+        );
     }
 }
 
